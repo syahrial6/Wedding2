ef50007d98db4b018948f7b313848b9dc91b377f
syahrial6 <syr06rial@gmail.com>
2023-07-18 14:54:24 +0700

    5

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['react-countdown'], // Tambahkan 'react-countdown' ke daftar external
+      external: ['/react-countdown/'], // Tambahkan 'react-countdown' ke daftar external
     },
   },
   plugins: [react()],
