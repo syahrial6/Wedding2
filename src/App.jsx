36656461a788920d6583e9b6348953c280978373
syahrial6 <syr06rial@gmail.com>
2023-07-25 13:49:15 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,62 @@
 import { Box, Text, Center, Image, Grid, Button, Link } from '@chakra-ui/react'
 import '@fontsource/rouge-script';
 import { animated, useSpring } from '@react-spring/web'
-import { useRef } from 'react';
-import Modals from './Modal';
+import { useRef, useState, useEffect } from 'react';
 import Countdown from 'react-countdown';
 import AnimatedComponent from './Animasi';
 import countdownRenderer from './CountDown';
-import foto from "/src/image/bg.jpg"
+import bunga2 from "/src/image/bunga2.png"
+import bunga1 from "/src/image/bunga1.png"
+import bingkai from "/src/image/bingkai.png"
 import bismillah from "/src/image/bismillah.png"
-import sound from "/src/music/sound.mp3"
+import sound from "/src/music/sound2.mp3"
 
 
 
 function App() {
-
+  const [isScrollEnabled, setScrollEnabled] = useState(false);
+  const [isButtonVisible, setButtonVisible] = useState(true);
   const audioRef = useRef();
+  const kontenRef = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+
+    };
+  }, [isScrollEnabled]);
 
 
-  // kode hitungan mundur
+  const toggleScroll = () => {
+    setScrollEnabled(!isScrollEnabled);
+  };
+
+  const handleScroll = () => {
+    if (!isScrollEnabled) {
+      // Menggulir ke atas jika scroll tidak diaktifkan
+      window.scrollTo(0, 0);
+    }
+  };
+
+  const scrollToKonten = () => {
+    if (kontenRef.current) {
+      kontenRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   const targetDate = new Date('July 29, 2023 12:00:00');
   const currentDate = new Date();
   const timeDifference = targetDate.getTime() - currentDate.getTime();
   const milliseconds = Math.max(timeDifference, 0);
 
-  
+
   // batas kode hitungan mundur
 
   const playAudio = () => {
     console.log("berhasil")
-    audioRef.current.play();
+    // audioRef.current.play();
+    setButtonVisible(false)
   };
 
 
@@ -38,11 +66,6 @@ function App() {
     config: { duration: 1300 },
   });
 
-  const bawahkeatas2 = useSpring({
-    from: { y: 100, opacity: 0 },
-    to: { y: 0, opacity: 1 },
-    config: { duration: 1300 },
-  });
   const fade2 = useSpring({
     from: { opacity: 0 },
     to: { opacity: 1 },
@@ -51,44 +74,76 @@ function App() {
 
 
 
-
-
   return (
     <>
       <audio src={sound} ref={audioRef} />
-      <Modals fungsi={playAudio} />
-      <Box
-        bgImage={foto}
+      <Box pb={"96"}
+        bgColor={"#b0d1b1"}
         bgPosition="center"
         bgSize={"cover"}
       >
+        <Image src={bunga2}
+          transform="translate(100%, 0%)"
+          width="50%" height="50%" objectFit="cover"></Image>
+
         <Center>
 
-          <Box py={"80"} color={"white"}>
+          <Box pt={"72"} pb={"32"} color={"white"} position={"absolute"}>
             <animated.div style={ataskebawah2}>
               <Text
                 textAlign={"center"}
                 fontSize={"3xl"}
-              >Undangan Pernikahan</Text>
+                fontWeight={"bold"}
+              >The Wedding Of</Text>
             </animated.div>
             <animated.div style={fade2}>
-              <Text textAlign={"center"}
-                fontWeight={"bold"} fontFamily="Rouge Script, cursive"
-                fontSize={"7xl"}>Nanda & Dian</Text>
-            </animated.div>
-            <animated.div style={bawahkeatas2}>
-              <Text textAlign={"center"} fontSize={"xl"}>Sabtu 29 Juli 2023</Text>
-              <Text textAlign={"center"} fontSize={"xl"}>Jam</Text>
-              <Text textAlign={"center"} fontSize={"xl"}>12.00 - 18.00 WIB</Text>
-              <Text pt={"14"} textAlign={"center"} fontSize={"xl"}>Pontianak Convention Center</Text>
-              <Text textAlign={"center"} fontSize={"xl"}>Jalan Sultan Syarif Abdurrahman</Text>
+              <Box position="relative" pb="6">
+                <Image src={bingkai} />
+                <Text py={"10"}
+                  color={"whiteAlpha.900"}
+                  position="absolute"
+                  top="50%"
+                  left="50%"
+                  transform="translate(-50%, -50%)"
+                  textAlign="center"
+                  fontWeight="bold"
+                  fontFamily="Rouge Script, cursive"
+                  fontSize="7xl"
+                  zIndex="5"
+                >
+                  Nanda  Dian
+                </Text>
+                <Text
+                  position="absolute"
+                  top="50%"
+                  left="50%"
+                  transform="translate(-50%, -50%)"
+                  textAlign="center"
+                  fontWeight="bold"
+                  fontFamily="Rouge Script, cursive"
+                  fontSize="7xl"
+                  zIndex="5"
+                >
+                  &
+                </Text>
+              </Box>
+              <Image src={bunga1}
+          transform="translate(0%, 100%)"
+          width="50%" height="50%" objectFit="cover"></Image>
+              <Center>
+                {isButtonVisible && (
+                  <Button href="#konten" onClick={() => { toggleScroll(); playAudio(); scrollToKonten(); }}>
+                    {isScrollEnabled ? null : "Buka Undangan"}
+                  </Button>
+                )}
+              </Center>
             </animated.div>
           </Box>
-
         </Center>
+
       </Box>
       <Box>
-        <Box py={"14"}>
+        <Box ref={kontenRef} py={"14"}>
           <Center>
             <AnimatedComponent animationConfig={() => ({
               from: {
@@ -148,82 +203,108 @@ function App() {
           </AnimatedComponent>
         </Box>
       </Box>
-      <AnimatedComponent  animationConfig={() => ({
-            from: {
-              opacity: 0,
-              y: 100,
-            },
-            to: {
-              opacity: 1,
-              y: 0,
-            },
-            rootMargin: '-30% 0%',
-            config: { duration: 1100 },
-          })}>
-      <Box bgColor={"#f4e2e2"}>
-        <Text py={"14"} textAlign={"center"} fontSize={"2xl"} fontWeight={"semibold"}>Waktu Pelaksanaan</Text>
-        <Box>
-          <Center>
+      <AnimatedComponent animationConfig={() => ({
+        from: {
+          opacity: 0,
+          y: 100,
+        },
+        to: {
+          opacity: 1,
+          y: 0,
+        },
+        rootMargin: '-30% 0%',
+        config: { duration: 1100 },
+      })}>
+        <Box bgColor={"#f4e2e2"}>
+          <Text py={"14"} textAlign={"center"} fontSize={"2xl"} fontWeight={"semibold"}>Waktu Pelaksanaan</Text>
+          <Box>
+            <Center>
 
-            <Grid templateColumns='repeat(2, 1fr)' gap={26} fontSize={"2xl"}>
+              <Grid templaterow='repeat(1, 1fr)' gap={26} fontSize={"2xl"}>
 
-              <Box textAlign={"center"} px={"14"} >
-                <Text>Sabtu</Text>
-                <Text fontSize={"4xl"} fontWeight={"bold"}>29</Text>
-                <Text>Juli 2023</Text>
-              </Box>
+                <Box textAlign={"center"} px={"14"} pb={"14"} >
+                  <Text fontFamily="Rouge Script, cursive" fontWeight={"bold"} fontSize={"4xl"}>Akad Nikah</Text>
+                  <Text fontSize={"2xl"}> Jumat 28 Juli 2023</Text>
+                  <Text>08.00 - Selesai</Text>
+                </Box>
+                <Box textAlign={"center"}>
+                  <Text fontFamily="Rouge Script, cursive" fontWeight={"bold"} fontSize={"4xl"}>Resepsi</Text>
+                  <Text>Sabtu 29 Juli 2023</Text>
+                  <Text>12.00 - 18.00 WIB</Text>
+                </Box>
+
+              </Grid>
+
+            </Center>
+            <Center>
               <Box textAlign={"center"}>
-                <Text>Resepsi</Text>
-                <Text>12.00 - 18.00 WIB</Text>
+                <Text fontWeight={"bold"} fontSize={"2xl"} pb="14">
+                  <Countdown date={Date.now() + milliseconds} renderer={countdownRenderer} />
+                </Text>
               </Box>
-
-            </Grid>
-
-          </Center>
-          <Center>
-            <Box textAlign={"center"}>
-              <Text fontWeight={"bold"} fontSize={"2xl"}>
-                <Countdown date={Date.now() + milliseconds} renderer={countdownRenderer} />
-              </Text>
-            </Box>
-          </Center>
+            </Center>
 
 
+          </Box>
         </Box>
-      </Box>
       </AnimatedComponent>
       <Box textAlign={"center"}>
-        <Box py={"14"}>
-          <Text fontSize={"3xl"} fontWeight={"semibold"}>LOKASI</Text>
-          <Text fontSize={"2xl"} fontWeight={"bold"}>Pontianak Convention Center</Text>
-        </Box>
-        <Box width={"100%"}>
-          <Center>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7752.470391595663!2d109.32790129324503!3d-0.03791217859314347!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e1d59aaa158addb%3A0x9916ae6426d80357!2sPontianak%20Convention%20Centre!5e0!3m2!1sid!2sid!4v1689660112312!5m2!1sid!2sid" width="600" height="450" style={{ border: '0', width: "70%" }} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-          </Center>
-        </Box>
-        <Button my={"14"} color={"white"} bgColor={"orange.600"}><Link href='https://goo.gl/maps/A34ZddrR9BpzqQmQ8' target='_blank'>Google Maps</Link></Button>
+        <AnimatedComponent animationConfig={() => ({
+          from: {
+            opacity: 0,
+            y: 100,
+          },
+          to: {
+            opacity: 1,
+            y: 0,
+          },
+          rootMargin: '-30% 0%',
+          config: { duration: 1100 },
+        })}>
+          <Box py={"14"}>
+            <Text fontSize={"3xl"} fontWeight={"semibold"}>LOKASI</Text>
+            <Text fontSize={"2xl"} fontWeight={"bold"}>Pontianak Convention Center</Text>
+          </Box>
+          <Box width={"100%"}>
+            <Center>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7752.470391595663!2d109.32790129324503!3d-0.03791217859314347!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e1d59aaa158addb%3A0x9916ae6426d80357!2sPontianak%20Convention%20Centre!5e0!3m2!1sid!2sid!4v1689660112312!5m2!1sid!2sid" width="600" height="450" style={{ border: '0', width: "70%" }} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            </Center>
+          </Box>
+          <Button my={"14"} color={"white"} bgColor={"orange.600"}><Link href='https://goo.gl/maps/A34ZddrR9BpzqQmQ8' target='_blank'>Google Maps</Link></Button>
+        </AnimatedComponent>
       </Box>
       <Box bgColor={"#f4e2e2"} >
-        <Text textAlign={"center"} py={"24"} fontSize={{ md: "xl", lg: "2xl" }}>
-          Merupakan suatu kehormatan dan kebahagiaan bagi kami
-          <br />
-          apabila Bapak/Ibu/Saudara/Saudari berkenan hadir
-          memberikan doa restu.
-          <br />
-          <br />
-          Jika bisa hadir kami tunggu konfirmasinya,
-          <br />
-          Informasi: Di meja penerima tamu akan kami sediakan hand sanitizer dan pemeriksaan suhu tubuh.
-          <br />
-          <br />
-          Jika tidak memungkinkan untuk hadir di pernikahan kami,tidak mengapa,
-          <br />
-          semoga bisa berjumpa di lain kesempatan
-          <br />
-          <br />
-          Stay safe & jaga kesehatan ya :)
-        </Text>
+        <AnimatedComponent animationConfig={() => ({
+          from: {
+            opacity: 0,
+            y: 100,
+          },
+          to: {
+            opacity: 1,
+            y: 0,
+          },
+          rootMargin: '-30% 0%',
+          config: { duration: 1100 },
+        })}>
+          <Text textAlign={"center"} py={"24"} fontSize={{ sm: "xl", md: "2xl", lg: "2xl" }}>
+            Merupakan suatu kehormatan dan kebahagiaan bagi kami
+            <br />
+            apabila Bapak/Ibu/Saudara/Saudari berkenan hadir
+            memberikan doa restu.
+            <br />
+            <br />
+            Jika bisa hadir kami tunggu konfirmasinya,
+            <br />
+
+            <br />
+            Jika tidak memungkinkan untuk hadir di pernikahan kami,tidak mengapa,
+            <br />
+            semoga bisa berjumpa di lain kesempatan
+            <br />
+            <br />
+            Stay safe & jaga kesehatan ya :)
+          </Text>
+        </AnimatedComponent>
       </Box>
     </>
   )
